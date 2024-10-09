f1d72409eac915dd5eefb7b45356592e3ca9dd12
@Nayan-09 <22052478@kiit.ac.in>
2024-10-09 23:19:36 +0530

    Your commit message

---
+++ javascript_part3/Q5.js
@@ -0,0 +1,4 @@
+//Strip leading and trailing spaces from a string.
+
+let str = "   Hello World!   ";
+console.log(str.trim()); // Output: "Hello World!"
---
+++ javascript_part3/Q6.js
@@ -0,0 +1,5 @@
+//Check if an element exists in an array or not.
+
+let array = [1, 2, 3, 4, 5];
+let element = 3;
+console.log(array.includes(element)); // Output: true if element exists, false if it doesn't
